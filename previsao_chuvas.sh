7e5020a5627b390f52168e66b4eb0ba2c5aeba4f
Deivid Duarte <48442376+deividduarte20@users.noreply.github.com>
2022-06-12 20:48:20 -0300

    Update previsao_chuvas.sh

--- previsao_chuvas.sh
+++ previsao_chuvas.sh
@@ -1,15 +1,16 @@
 #!/usr/bin/env bash
-
-# previsa_chuvas.sh url - Extrai quantidade em mm de chuvas da url da cidade
+#
+# previsa_chuvas.sh - Extrai quantidade em mm de chuvas da url da cidade
 #
 # Site:       https://www.youtube.com/c/CaindonaRede
 # Autor:      Deivid Duarte
 # Manutenção: Deivid Duarte
 #
 # ------------------------------------------------------------------------ #
-#  Este programa irá extrair a preciptação de chuvas
+#  Este programa irá extrair a preciptação de chuvas para pegar a url acesse o site https://www.climatempo.com.br/previsao-do-tempo?page=HOJE 
+pesquise a cidade e copie e cole ao lado do nome do script conforme segue exemplo abaixo.
 #  Exemplos:
-#      $ ./previsao_chuva.sh url
+#      $ ./previsao_chuvas.sh url
 #
 # ------------------------------------------------------------------------ #
 # Histórico:
@@ -19,12 +20,11 @@
 # ------------------------------------------------------------------------ #
 # Testado em:
 #   bash 4.19
-# ------------------------------------------------------------------------ #
 #
-# ------------------------------- TESTES ----------------------------------------- #
+# ------------------------------- TESTES --------------------------------- #
 [ ! -x "$(which lynx)" ] && sudo apt install lynx -y # Lynx instalado?
 # ------------------------------------------------------------------------ #
-
+#
 # ------------------------------- EXECUÇÃO ----------------------------------------- #
 STATUS=`lynx -source $1 | grep _margin-l-5 | cut -d ">" -f2 | cut -d "-" -f1 | sed 4,5d | sed 1,2d`
 echo $STATUS
